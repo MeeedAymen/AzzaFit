6adc35eee743af5726f51ba889c543190ee5afe5
Mohamed Aymen Azza <mohamedaymenazza@gmail.com>
2023-10-25 21:27:55 +0100

    responcive

---
+++ index.js
@@ -0,0 +1,19 @@
+const questions = document.querySelectorAll('.question');
+        var barMenu = document.querySelector(".bar-menu");
+        var closebtn = document.querySelector(".close")
+        var ul = document.querySelector("ul");
+        questions.forEach(question => 
+        { question.addEventListener('click', () => 
+            {const   answer = question.querySelector('.answer');
+                if (answer.style.display == 'none'){
+                    answer.style.display = 'block';
+                }
+                else {answer.style.display = 'none';}
+            });
+        });
+        barMenu.addEventListener('click',e =>{
+            ul.style.left = 0;
+        })
+        closebtn.addEventListener('click',e=>{
+            ul.style.left = "-300px";
+        })
